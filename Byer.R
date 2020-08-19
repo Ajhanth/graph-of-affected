14e1ce84693f6a9281ad2c1e93f9170d4e49c85f
Ajhanth <69298832+Ajhanth@users.noreply.github.com>
2020-08-19 18:19:39 +0200

    3rd commit

---
+++ Byer.R
@@ -0,0 +1,55 @@
+# In this script I have made a graph that shows how many people are affected by Covid-19 in different cities in Norway
+
+# Bergen smittede i augugst
+
+x1=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14)
+s1=c(0,1,3,0,2,2,4,0,0,1,1,0,5,0)
+
+dagerb <- x1
+
+sm <- s1
+
+# Oslo smittede i augugst
+
+x2=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14)
+s2=c(3,3,13,10,17,21,34,14,6,11,21,7,18,21)
+
+so <- s2
+
+# Stavanger smittede i augugst
+
+x3=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14)
+s3=c(0,0,1,0,2,1,2,0,0,1,1,0,0,2)
+
+ss <- s3
+
+
+# Trondheim smittede i augugst
+
+h=c(-3,-3,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14)
+s3=c(0,1,1,2,1,1,4,1,4,1,3,2,0,2)
+
+dagb <- h
+
+st <- s3
+
+# Graf av smittede
+
+
+plot(dagb,type = "o", lwd=2,
+     xaxt="n",ylim =c(0,39),col="black",
+     xlab = "Første 14 dagene i august",
+     ylab = "Smiitede for hver dag",
+     main = "Sammenligning av byer")
+axis(1,at=1: length(dagerb), labels = dagerb)
+lines(sm,col="red",type ="b",lwd=2,pch = c(20))
+lines(so,col="blue",type ="b",lwd=2,pch = c(20))
+lines(ss,col="yellow",type ="b",lwd=1,pch = c(20))
+lines(st,col="green",type ="b",lwd=2,pch = c(20))
+
+legend("topleft",
+       legend = c("Bergen","Oslo","Stavanger","Trondheim"),
+       col = c("red","blue","yellow","green"),
+       pch = c(20)
+      )
+
